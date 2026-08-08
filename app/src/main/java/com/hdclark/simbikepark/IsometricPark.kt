932ec874baed58b21e2a1a499158fb8e58126dce
Hal Clark <hdclark@ualberta.ca>
2026-08-08 12:51:16 -0600

    Fix Compose native canvas import

--- app/src/main/java/com/hdclark/simbikepark/IsometricPark.kt
+++ app/src/main/java/com/hdclark/simbikepark/IsometricPark.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import kotlin.math.floor
 import kotlin.math.sin
